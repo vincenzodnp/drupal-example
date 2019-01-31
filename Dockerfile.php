b1e735d14fde0561230816afe333f2fa6ae4dadc
Vincenzo <vincenzo.denaropapa@gmail.com>
2019-01-31 04:48:48 +0100

    Added php ldap extension to project

--- Dockerfile.php
+++ Dockerfile.php
@@ -4,3 +4,8 @@ FROM ${CLI_IMAGE} as cli
 FROM amazeeio/php:7.2-fpm
 
 COPY --from=cli /app /app
+
+#RUN apk add --no-cache openldap-dev
+#RUN docker-php-ext-install ldap
+RUN apk add --no-cache php7-ldap
+RUN docker-php-ext-enable /usr/lib/php7/modules/ldap.so
